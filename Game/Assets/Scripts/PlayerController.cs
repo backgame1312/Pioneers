733d717653629efbdba5d2dd6e7515783b45f3a3
backgame1312 <bttibeti@gmail.com>
2025-04-14 21:49:28 +0900

    플레이어 이동/로직 구현

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float deathY = -10f;
+    [Header("Respawn Settings")]
+    public float deathY = -10.0f;
     private Vector2 respawnPosition = new Vector2(-5f, -2f);
     private int deathCount = 0;
 
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5.0f;
+    public float jumpForce = 5.0f;
+
+    [Header("Jump Tuning")]
+    public float defaultjump = 2f;
+    public float jumpSpeed = 4.0f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
+    {
+        MovementController();
+        JumpGravity();
+        CheckFallDeath();
+    }
+
+    private void MovementController()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void JumpGravity()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = jumpSpeed;
+        }
+        else
+        {
+            rb.gravityScale = defaultjump;
+        }
+    }
+
+    private void CheckFallDeath()
     {
         if (transform.position.y < deathY)
         {
@@ -24,15 +68,20 @@ public class PlayerController : MonoBehaviour
     void Die()
     {
         deathCount++;
+        Debug.Log("ÇÃ·¹ÀÌ¾î°¡ Á×¾ú½À´Ï´Ù. Á×Àº È½¼ö: " + deathCount);
 
         transform.position = respawnPosition;
 
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (rb != null)
-        {
-            rb.velocity = Vector2.zero;
-        }
+        rb.velocity = Vector2.zero;
 
         UIManager.Instance.UpdateDeathCount(deathCount);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
