053d2c8d7c8e6f922c18331f23787284a77059d8
H_Hyun <127277391+backgame1312@users.noreply.github.com>
2025-04-29 11:51:53 +0900

    Create UIManager.cpp

---
+++ Scripts/UIManager.cpp
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    public TextMeshProUGUI death;
+    public GameObject deathGameObject;
+
+    public TextMeshProUGUI elapseTime;
+    public GameObject elapseTimeGameObject;
+
+    public GameObject mainMenu;
+
+    public GameObject pauseUI; 
+    public GameObject resumeButton; 
+    public GameObject returnMainMenuButton; 
+    private bool isPaused = false;
+
+    private int deathCount = 0;
+    private float elapsed = 0.0f;
+
+    public GameObject tutorialUI;
+    public Button buttonA, buttonD, buttonE, buttonSpace;
+    private bool isFirstDeath = false;
+
+    Color normal = new Color(1, 1, 1, 0.5f); // 반투명
+    Color pressed = new Color(1, 1, 1, 1f);  // 불투명
+
+    void Start()
+    {
+        pauseUI.SetActive(false);
+        mainMenu.SetActive(true);
+        Time.timeScale = 0;
+
+        deathGameObject.gameObject.SetActive(false);
+        elapseTimeGameObject.gameObject.SetActive(false);
+
+        tutorialUI.SetActive(false);
+
+        AudioManager.Instance.StopBGM();
+    }
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateElapsedTime();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+
+        UpdateButton(buttonA, KeyCode.A);
+        UpdateButton(buttonD, KeyCode.D);
+        UpdateButton(buttonE, KeyCode.E);
+        UpdateButton(buttonSpace, KeyCode.Space);
+    }
+
+    private void UpdateElapsedTime()
+    {
+        elapsed += Time.deltaTime;
+
+        int hours = Mathf.FloorToInt(elapsed / 3600f);
+        int minutes = Mathf.FloorToInt((elapsed % 3600f) / 60f);
+        int seconds = Mathf.FloorToInt(elapsed % 60f);
+
+        elapseTime.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+    }
+
+    public void UpdateDeathCount(int count)
+    {
+        deathCount = count;
+        death.text = " " + deathCount;
+    }
+
+    public void OnStartButtonClicked()
+    {
+        Time.timeScale = 1;
+        mainMenu.SetActive(false);
+
+        deathGameObject.gameObject.SetActive(true);
+        elapseTimeGameObject.gameObject.SetActive(true);
+
+        if (!isFirstDeath)
+            tutorialUI.SetActive(true);
+
+        AudioManager.Instance.PlayButtonClick();
+        AudioManager.Instance.PlayBGM();
+    }
+
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+
+        AudioManager.Instance.PlayButtonClick();
+    }
+
+    void TogglePauseMenu()
+    {
+        isPaused = !isPaused;
+        pauseUI.SetActive(isPaused);
+        mainMenu.SetActive(false);
+        Time.timeScale = isPaused ? 0 : 1;
+        tutorialUI.SetActive(false);
+
+        if (isPaused)
+        {
+            AudioManager.Instance.StopBGM();
+        }
+        else
+        {
+            AudioManager.Instance.PlayBGM();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        TogglePauseMenu();
+
+        AudioManager.Instance.PlayButtonClick();
+    }
+
+    public void ReturnMainMenuButton()
+    {
+        Time.timeScale = 0;
+
+        PlayerController player = FindObjectOfType<PlayerController>();
+        if (player != null)
+        {
+            player.transform.position = player.startingPosition; 
+        }
+
+        pauseUI.SetActive(false);
+        mainMenu.SetActive(true);
+
+        deathGameObject.gameObject.SetActive(false);
+        elapseTimeGameObject.gameObject.SetActive(false);
+
+        AudioManager.Instance.PlayButtonClick();
+        AudioManager.Instance.PlayBGM();
+    }
+
+    void UpdateButton(Button btn, KeyCode key)
+    {
+        var colors = btn.colors;
+        colors.normalColor = Input.GetKey(key) ? pressed : normal;
+        btn.colors = colors;
+    }
+
+    public void OnPlayerDeath()
+    {
+        if (!isFirstDeath)
+        {
+            tutorialUI.SetActive(false);
+            isFirstDeath = true;
+        }
+    }
+}
